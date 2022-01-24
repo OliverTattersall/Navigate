20aef41788e588ccacb9d982b7e500e37ffa0e79
Oliver Tattersall <oliver.tattersall@olivertattersall.local>
2022-01-24 14:04:33 -0500

    add auth

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Leaflet Js
+
+var rock=true;
+
+
 var mymap = L.map('map',{
   zoomControl:true
 }).setView([44.552923140196725, -78.15305721293893], 13);
@@ -26,12 +31,19 @@ var lat, lng;
 mymap.addEventListener('click', (ev)=>{
     lat = ev.latlng.lat;
     lng = ev.latlng.lng;
-    markers.push(L.marker([lat, lng], {icon:iconval}).addTo(mymap))
-    console.log(lat, lng)
-    console.log(markers[0])
+    if(rock){
+      markers.push(L.marker([lat, lng], {icon:iconval}).addTo(mymap))
+      console.log(lat, lng)
+      console.log(markers[0])
+    }
     // markers[0]["_icon"] = "<img src='images/marker-icon copy.png'>"
     // console.log(markers[0]["_icon"])
 })
+
+
+
+// Materialize Js
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
@@ -42,3 +54,94 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems);
 });
+
+
+
+// Firebase Js
+
+const config = {
+  apiKey: "AIzaSyCx3XEp_L0OViWBsb4uSCi76hAkiiBmnTU",
+  authDomain: "navigate-5ba1a.firebaseapp.com",
+  databaseURL: "https://navigate-5ba1a-default-rtdb.firebaseio.com",
+  projectId: "navigate-5ba1a",
+  storageBucket: "navigate-5ba1a.appspot.com",
+  messagingSenderId: "195224192661",
+  appId: "1:195224192661:web:b3a5aa8a1769fb0afb7d9c",
+  measurementId: "G-R8PRK1EG56"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+const database = firebase.database();
+
+
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+firebase.auth().onAuthStateChanged((user) => {
+  console.log("hello")
+  if (user) {
+      console.log("in")
+      console.log(user.uid)
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      database.ref('users/'+uid).once('value',(v)=>{
+        d = v.val()
+        vals = [d['UserName'], d['email'], d['HomeLocation'], d['Location']]
+        updateInfo(vals)
+      } )
+      
+      
+      // ...
+  } else {
+      console.log("no")
+      database.ref('users/test').once('value',(v)=>{
+        d = v.val()
+        vals = [d['UserName'], d['email'], d['HomeLocation'], d['Location']]
+        updateInfo(vals)
+      } )
+      // User is signed out
+      // ...
+  }
+});
+
+
+function updateInfo(data){
+
+  userInfo = document.getElementsByClassName("info")
+
+  
+  userInfo[0].innerHTML +=" "+data[0]
+  userInfo[1].innerHTML +=" "+data[1]
+  if(data[2]==="false"){
+    userInfo[2].innerHTML +=" Not Set"
+  }else{
+    userInfo[2].innerHTML +=" Set"
+  }
+
+  if(data[3]==="false"){
+    userInfo[3].innerHTML +=" Inactive"
+  }else{
+    userInfo[3].innerHTML +=" Active"
+  }
+  
+
+}
+
+
+
+
+// updateInfo(["Oliver", "test@test.com", "Not Set", "Inactive"])
